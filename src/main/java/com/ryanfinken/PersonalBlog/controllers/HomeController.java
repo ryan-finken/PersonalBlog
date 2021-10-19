b150c8ec59b41ee915445ee21a39b472e0d6e1b1
Ryan Finken <ryanfinken338@gmail.com>
2021-10-19 13:00:30 -0500

    created home controller and testable index template

---
+++ src/main/java/com/ryanfinken/PersonalBlog/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package com.ryanfinken.PersonalBlog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping
+    public String displayIndex() {
+        return "index";
+    }
+}
