ffe60f3876ff0284a4226b497f987eca59d6af7a
Ryan Finken <ryanfinken338@gmail.com>
2021-10-20 13:40:32 -0500

    created models package. added AbstractEntity class to models package

---
+++ src/main/java/com/ryanfinken/PersonalBlog/models/AbstractEntity.java
@@ -0,0 +1,28 @@
+package com.ryanfinken.PersonalBlog.models;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+public abstract class AbstractEntity {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AbstractEntity)) return false;
+        AbstractEntity that = (AbstractEntity) o;
+        return getId() == that.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
