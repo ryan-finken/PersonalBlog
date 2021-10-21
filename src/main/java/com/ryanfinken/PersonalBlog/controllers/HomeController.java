7f9569d09b02de1d2ee9553b50195445b1d9b515
Ryan Finken <ryanfinken338@gmail.com>
2021-10-21 15:28:01 -0500

    removed unused imports

--- src/main/java/com/ryanfinken/PersonalBlog/controllers/HomeController.java
+++ src/main/java/com/ryanfinken/PersonalBlog/controllers/HomeController.java
@@ -1,14 +1,10 @@
 package com.ryanfinken.PersonalBlog.controllers;
 
 import com.ryanfinken.PersonalBlog.data.PostRepository;
-import com.ryanfinken.PersonalBlog.models.Post;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 @Controller
 public class HomeController {
