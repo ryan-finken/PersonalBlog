1dcf26f4568bf6f3ac1e822dc38014126b778d17
Ryan Finken <ryanfinken338@gmail.com>
2021-10-21 13:46:35 -0500

    added descending order sort function to PostRepository

--- src/main/java/com/ryanfinken/PersonalBlog/controllers/HomeController.java
+++ src/main/java/com/ryanfinken/PersonalBlog/controllers/HomeController.java
@@ -1,20 +1,23 @@
 package com.ryanfinken.PersonalBlog.controllers;
 
 import com.ryanfinken.PersonalBlog.data.PostRepository;
+import com.ryanfinken.PersonalBlog.models.Post;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
     @Autowired
-    public PostRepository posts;
+    public PostRepository postRepository;
 
     @RequestMapping
     public String displayIndex(Model model) {
-        model.addAttribute("posts", posts.findAll());
+        model.addAttribute("posts", postRepository.findAllByOrderByTimestampDesc());
         return "index";
     }
 }
--- src/main/java/com/ryanfinken/PersonalBlog/data/PostRepository.java
+++ src/main/java/com/ryanfinken/PersonalBlog/data/PostRepository.java
@@ -3,5 +3,8 @@ package com.ryanfinken.PersonalBlog.data;
 import com.ryanfinken.PersonalBlog.models.Post;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface PostRepository extends CrudRepository<Post, Integer> {
+    public List<Post> findAllByOrderByTimestampDesc();
 }
