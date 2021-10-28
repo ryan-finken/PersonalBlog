31a8dae87b7dad2896bfffda19b6658fefd88466
Ryan Finken <ryanfinken338@gmail.com>
2021-10-28 12:47:17 -0500

    created handlers to display and process delete posts form in AdminController. created view for delete posts form, and added a link to that view in admin index

--- src/main/java/com/ryanfinken/PersonalBlog/controllers/AdminController.java
+++ src/main/java/com/ryanfinken/PersonalBlog/controllers/AdminController.java
@@ -5,10 +5,7 @@ import com.ryanfinken.PersonalBlog.models.Post;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("admin")
@@ -30,6 +27,23 @@ public class AdminController {
     @PostMapping("create")
     public String processCreatePostForm(@ModelAttribute Post post) {
         postRepository.save(post);
+        return "redirect:/";
+    }
+
+    @GetMapping("delete")
+    public String displayDeletePostsForm(Model model) {
+        model.addAttribute("posts", postRepository.findAll());
+        return "admin/delete";
+    }
+
+    @PostMapping("delete")
+    public String processDeletePostsForm(@RequestParam(required = false) int[] postIds) {
+        if (postIds != null) {
+            for (int id : postIds) {
+                postRepository.deleteById(id);
+            }
+        }
+
         return "redirect:/";
     }
 }
